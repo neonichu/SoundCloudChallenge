f61cb398637448a3bc7e8bd41f6add3d1df6c97d
Boris Bügling <boris@icculus.org>
2013-07-14 23:32:11 +0200

    Implemented unit test for class BBUTrackItem.

--- Tests/SoundCloudChallengeTests.m
+++ Tests/SoundCloudChallengeTests.m
@@ -11,10 +11,14 @@
 #else 
 #import <SenTestingKit/SenTestingKit.h>
 
+#define XCTAssertEqualObjects STAssertEqualObjects
+#define XCTAssertNil STAssertNil
 #define XCTFail STFail
 
 typedef SenTestCase XCTestCase;
 #endif
+
+#import "BBUTrackItem.h"
 
 @interface SoundCloudChallengeTests : XCTestCase
 
@@ -24,23 +28,21 @@ typedef SenTestCase XCTestCase;
 
 @implementation SoundCloudChallengeTests
 
-- (void)setUp
-{
-    [super setUp];
+-(void)testTrackItem {
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"SampleTrackResponse" ofType:@"json"];
+    NSData* data = [NSData dataWithContentsOfFile:path];
+    NSDictionary* dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    BBUTrackItem* trackItem = [BBUTrackItem trackItemWithDictionary:dictionary];
     
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertNil(trackItem.artworkURL, @"Error in artworkURL");
+    XCTAssertEqualObjects([NSURL URLWithString:@"https://i1.sndcdn.com/avatars-000006288728-hy3g41-large.jpg?cc07a88"],
+                          trackItem.avatarURL, @"Error in avatarURL");
+    XCTAssertEqualObjects(@(100769720), trackItem.identifier, @"Error in identifier");
+    XCTAssertEqualObjects([NSURL URLWithString:@"http://soundcloud.com/scott-capurro/test-run-of-comedy"],
+                          trackItem.permalinkURL, @"Error in permalinkURL");
+    XCTAssertEqualObjects(@"Test run of comedy", trackItem.title, @"Error in title");
+    XCTAssertEqualObjects([NSURL URLWithString:@"https://w1.sndcdn.com/L6v9gN4tkbpq_m.png"],
+                          trackItem.waveformURL, @"Error waveformURL");
 }
 
 @end
